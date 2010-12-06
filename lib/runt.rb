5e80fd7cc5b4e7b9be17308056b7bbb7910e8fcb
U-DALLBEN\Admin <Admin@DALLBEN.(none)>
2010-12-06 11:27:27 -0800

    Fix for ruby 1.9

--- lib/runt.rb
+++ lib/runt.rb
@@ -87,10 +87,10 @@ module Runt
       elsif (11..13).include?(number.to_i % 100)
 	"#{number}th"
       else
-	case number.to_i % 10
-	  when 1: "#{number}st"
-	  when 2: "#{number}nd"
-	  when 3: "#{number}rd"
+	case "#{number.to_i%10}"
+	  when "1": "#{number}st"
+	  when "2": "#{number}nd"
+	  when "3": "#{number}rd"
 	  else    "#{number}th"
 	end
       end
